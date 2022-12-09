d4b85f4650d8289a8c83586caa01d3403aaeccfb
Steve Pagno <105939669+523580605@users.noreply.github.com>
2022-12-09 09:03:10 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var img = document.querySelector("#image");
+
+function trocar() {
+    img.setAttribute('src', 'https://s2.glbimg.com/9moAsJlQxE2gQ7hk9nP1ZoEo51A=/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2022/P/E/Q4oLrGTJGgIXDRrXBaFA/ff666214-63bd-41ad-91b2-a8601f0d415e.jpg');
+}
